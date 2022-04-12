385a00dcfc4abfa87f2b147e8fd43a30e938f809
Aryan Dash <aryan.dash2019@vitstudent.ac.in>
2022-04-12 09:33:33 +0530

    .

---
+++ TREES/LC_98_VALIDATE_BINARY_SEARCH_TREES.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    bool solve(TreeNode* root, long long int min,long long int max){
+        if(!root)return true;
+        
+        if(root->val>=max || root->val<=min)return false;
+        
+        return solve(root->left,min,root->val) and solve(root->right,root->val,max);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        if(!root)return true;
+        
+        return solve(root,-1e18,1e18);
+    }
+};
+
+// TC: O(N)
+// SC: O(1)
