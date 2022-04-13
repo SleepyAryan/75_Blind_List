c122ff18f881c3111735b2326f42123c5bc4fcf3
Aryan Dash <aryan.dash2019@vitstudent.ac.in>
2022-04-13 10:28:37 +0530

    .

---
+++ LL/LC_206_REVERSE.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* current=head;
+        ListNode* next = current->next;
+        while(current!=NULL){
+            next=current->next;
+            current->next=prev;
+            prev=current;
+            current=next;
+        }
+        return prev;
+    }
+};
+
+// TC: O(N)
+// SC: O(N)
